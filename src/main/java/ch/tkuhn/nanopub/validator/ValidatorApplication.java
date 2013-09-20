dec39638343d4ed1966ea82f438983187e06e1b4
Tobias Kuhn <kuhntobias@gmail.com>
2013-09-20 18:05:49 +0200

    Plain web-application

---
+++ src/main/java/ch/tkuhn/nanopub/validator/ValidatorApplication.java
@@ -0,0 +1,17 @@
+package ch.tkuhn.nanopub.validator;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+public class ValidatorApplication extends WebApplication {    	
+
+	@Override
+	public Class<? extends WebPage> getHomePage() {
+		return ValidatorPage.class;
+	}
+
+	public void init() {
+		super.init();
+	}
+
+}
---
+++ src/main/java/ch/tkuhn/nanopub/validator/ValidatorPage.java
@@ -0,0 +1,13 @@
+package ch.tkuhn.nanopub.validator;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class ValidatorPage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+	public ValidatorPage(final PageParameters parameters) {
+		super(parameters);
+    }
+}
